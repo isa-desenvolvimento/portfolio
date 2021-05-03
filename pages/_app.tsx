eea4382d9c7800f065ea6024fcdebd5472967209
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-03 14:31:20 -0300

    feat: Conf iniciais

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default MyApp;
