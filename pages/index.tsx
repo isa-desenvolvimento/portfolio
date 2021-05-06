65c8f082636c0fa135783d1adf10e9f03133d748
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-05 22:12:59 -0300

    feat: Cat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
+import Lottie from 'react-lottie';
 import styles from '../styles/Home.module.scss';
 import Glass from './components/glass';
 import { animateRotate } from './utils/animateCard';
+import animationData from './utils/animationData.json';
 
 export default function Home() {
   const txt = 'Tenho 26 anos, moro em brasília, tenho o gato mais lindo do mundo, gosto de gravar vídeos e jogar airsoft ☠️';
@@ -9,10 +11,25 @@ export default function Home() {
   useEffect(() => {
     animateRotate('.container', '.glass-profile', ['.avatar']);
   });
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
     <div className={styles.container}>
       <div className={`container ${styles.glass}`}>
         <Glass title="Andressa Novaes" avatar="/img/avatar.jpeg" txt={txt} />
+        <Lottie
+          options={defaultOptions}
+          height={400}
+          width={400}
+        />
       </div>
     </div>
   );
